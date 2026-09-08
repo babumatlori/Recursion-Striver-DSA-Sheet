caabec5beeaac0f23cf06d70df00a317d7c43c24
Babu Matlori <babumatlori@gmail.com>
2026-09-08 19:28:21 +0530

    Recurison Introduction first Problem

---
+++ Introduction_01/Recrison1.java
@@ -0,0 +1,16 @@
+package Introduction_01;
+
+public class Recrison1 {
+
+   public static void fun(int n) {
+            if( n==0 ) {
+                return;
+            }
+            fun(n-1);
+             System.out.println(n);
+    }
+
+    public static void main(String[] args) {
+        fun(3);
+    }
+}
