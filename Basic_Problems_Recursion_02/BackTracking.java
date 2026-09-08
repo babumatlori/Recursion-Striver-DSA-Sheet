0c2ee52b71f01da280a3c75a9f7807b05f36cdd4
Babu Matlori <babumatlori@gmail.com>
2026-09-08 20:32:00 +0530

    Solved the backtracking from n to 1

--- Basic_Problems_Recursion_02/BackTracking.java
+++ Basic_Problems_Recursion_02/BackTracking.java
@@ -2,22 +2,22 @@ package Basic_Problems_Recursion_02;
 
 import java.util.Scanner;
 
-//print lineraly from n to 1
-public class PrintLineary {
+// n to 1
+public class BackTracking {
+
     static void printLin(int i, int n) {
-        if( i < 1) return;
-        // System.out.println(i);
-        printLin(i -1, n);
+        if( i > n) return ;
+        printLin( i + 1, n);
         System.out.println(i + " ");
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner( System.in);
-        System.out.println("enter a psotive number: ");
+          System.out.println("enter a psotive number: ");
         if (sc.hasNextInt()) {
             int n = sc.nextInt();
             System.out.println("Number from 1 to " + n + " are :" );
-            printLin(n, 1);
-        }
+            printLin(1, n);
+             }
         sc.close();
-    }
+         }
 }
